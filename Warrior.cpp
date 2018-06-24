91f3d5cfb7c41fb38debc9dd5ee7fa75a94f017c
almogtzabari <almogtzabari@campus.technion.ac.il>
2018-06-24 23:06:21 +0300

    updates

--- Warrior.cpp
+++ Warrior.cpp
@@ -1,2 +1,11 @@
 
 #include "Warrior.h"
+
+Warrior::Warrior(string const& name, Weapon const& weapon, bool rider):
+Player(name,weapon), mounted(rider){}
+
+void Warrior::makeStep() {
+    if(mounted){
+        position
+    }
+}
