a89539c4e313f69c0b95f17990afaced9c2f31a3
Ariel Kfir <Arielkfir@campus.technion.ac.il>
2018-06-24 23:07:54 +0200

    push

--- Wizard.h
+++ Wizard.h
@@ -2,8 +2,13 @@
 #ifndef MTM_EX5_WORLD_OF_HOMEWORK_WIZARD_H
 #define MTM_EX5_WORLD_OF_HOMEWORK_WIZARD_H
 
+#include "Player.h"
 
-class Wizard {
+class Wizard : public Player {
+    int range;
+
+public:
+    Wizard(string const& name,Weapon const& weapon, int range);
 
 };
 
